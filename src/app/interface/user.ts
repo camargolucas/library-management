558b7cc9776a06b933b4164df86c13d396c9db71
Lucas Camargo <lucas.camargo.silva98@gmail.com>
2023-03-08 22:20:46 -0300

    new feature

---
+++ src/app/interface/user.ts
@@ -0,0 +1,12 @@
+import { Books } from './books';
+
+export interface User {
+
+    name: string,
+    cpf: string,
+    books?: Array<Books>,
+    createdAt?:Date,
+    updatedAt?:Date
+}
+
+
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import packageJson from 'package.json';
+import { User } from 'src/app/interface/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,10 @@ import packageJson from 'package.json';
 export class LoginComponent implements OnInit {
   version: string = packageJson.version;
   formGroup: FormGroup;
+  signUpMode: boolean = false;
 
-  constructor(private router:Router) {
+
+  constructor(private router: Router, private userService: UserService) {
 
     this.formGroup = new FormGroup({
       name: new FormControl('', Validators.required),
@@ -24,29 +28,44 @@ export class LoginComponent implements OnInit {
 
   }
 
+  setSignupMode(mode: boolean) {
+    this.signUpMode = mode;
+  }
+
   validateForm() {
     const controls = ['name', 'cpf'];
     const fmControls = this.formGroup.controls;
-    controls.forEach(control => {
-      if (fmControls[control].invalid){        
-        fmControls[control].markAsDirty()
-        fmControls[control].markAsTouched()
-      }else{
-        this.login();
-      }
-      
-    });
+    if (this.formGroup.invalid) {
+      controls.forEach(control => {
+        if (fmControls[control].invalid) {
+          fmControls[control].markAsDirty()
+          fmControls[control].markAsTouched()
+        }
+      });
+    } else {
+      const user:User = this.formGroup.value;
+      this.login(user);
+    }
   }
 
-  login(){
-    this.navigate('home')
+
+
+  login(user: User) {
+    this.userService.login(user)
+      .subscribe(ret => {
+        console.log(ret);
+        this.navigate('')
+      }, error => {
+
+      })
+
   }
 
-    navigate(path:string){
-      this.router.navigate([`/${path}`], { replaceUrl: true });
+  navigate(path: string) {
+    this.router.navigate([`/${path}`], { replaceUrl: true });
   }
 
-  getFormControl(control:string){
+  getFormControl(control: string) {
     return this.formGroup.controls[control];
   }
 
